f084e63d8269d77c9068d22909c700a4940fca82
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-05-21 17:59:43 +0200

    save image to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use image::{ImageBuffer, Rgb, RgbImage};
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: u32 = 400;
+    const HEIGHT: u32 = 300;
+
+    let mut image: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let r: f64 = ((x as f64) / WIDTH as f64) * 256.0;
+            let g: f64 = ((y as f64) / WIDTH as f64) * 256.0;
+            let b: f64 = 0.25 * 256.0;
+
+            image.put_pixel(x, y, Rgb([r as u8, g as u8, b as u8]));
+        }
+    }
+
+    image.save("target/image.png").unwrap();
 }
